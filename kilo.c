58ec75c82bf47428af6aa034debe80649e519936
Sahak99 <sahakmatevosyan1999@gmail.com>
2022-02-19 17:22:05 +0400

    Home & End keys

--- kilo.c
+++ kilo.c
@@ -18,6 +18,9 @@ enum editorKey
     ARROW_RIGHT,
     ARROW_UP,
     ARROW_DOWN,
+    DEL_KEY,
+    HOME_KEY,
+    END_KEY,
     PAGE_UP,
     PAGE_DOWN,
 };
@@ -90,8 +93,13 @@ int editorKeyRead()
                 {
                     switch (seq[1])
                     {
-                    case '5': return PAGE_UP;
-                    case '6': return PAGE_DOWN;
+                        case '1': return HOME_KEY;
+                        case '3': return DEL_KEY;
+                        case '4': return END_KEY;
+                        case '5': return PAGE_UP;
+                        case '6': return PAGE_DOWN;
+                        case '7': return HOME_KEY;
+                        case '8': return END_KEY;
                     }
                 }
             } else {
@@ -101,9 +109,18 @@ int editorKeyRead()
                         case 'B': return ARROW_DOWN;
                         case 'C': return ARROW_RIGHT;
                         case 'D': return ARROW_LEFT;
+                        case 'H': return HOME_KEY;
+                        case 'F': return END_KEY;
                     }
                 }
-        }
+        }   else if(seq[0] == 'O')
+                {
+                    switch (seq[1])
+                    {
+                    case 'H': return HOME_KEY;
+                    case 'F': return END_KEY;
+                    }
+                }
 
     return '\x1b';
 
@@ -209,6 +226,13 @@ void editorProcessKeypress()
         write(STDOUT_FILENO, "\x1b[2J",4);
         write(STDOUT_FILENO, "\x1b[H",3);
         exit(0);
+        break;
+
+    case HOME_KEY:
+        E.cx = 0;
+        break;
+    case END_KEY:
+        E.cx = E.screencols - 1;
         break;
 
     case PAGE_UP:
